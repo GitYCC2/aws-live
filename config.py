ab545b9965a7c037ac5c6dea8e11358ea03079de
GitYCC2 <93185619+GitYCC2@users.noreply.github.com>
2022-04-15 13:06:26 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.czfxtgcswdmu.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "capc3009"
+customdb = "employee"
+custombucket = "yongchichun-bucket"
+customregion = "us-east-1"
